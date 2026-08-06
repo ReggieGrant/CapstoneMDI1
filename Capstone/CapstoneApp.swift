a3cfe416955e036c514590deb7b72601c8499ab0
ReggieGrant <grantreggie09@gmail.com>
2026-08-05 17:10:05 -0700

    Initial Commit

---
+++ Capstone/CapstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapstoneApp.swift
+//  Capstone
+//
+//  Created by Reginald Grant on 8/5/26.
+//
+
+import SwiftUI
+
+@main
+struct CapstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
